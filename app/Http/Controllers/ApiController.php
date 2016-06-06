0f1126ed2094ad9fc5b30f2e52989d7515ead7f0
javipedrera <javierpedrerajimenez@gmail.com>
2016-06-06 23:52:31 +0200

    Authentizer added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,10 +3,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Response;
+use App\Http\Controllers\AuthenticateController;
 
 class ApiController extends BaseController {
-	
+
 	protected $statusCode = 200;
+	protected $authentizer;
+
+	public function __construct(AuthenticateController $authentizer) {
+		$this->authentizer = $authentizer;
+	}
 
 	public function getStatusCode()
 	{
@@ -20,32 +26,9 @@ class ApiController extends BaseController {
 		return $this;
 	}
 
-	public function respondNotFound($message = 'Not found')
-	{
-		return $this->setStatusCode(404)->respondWithError($message);
-	}
-
-	public function respondInternalError($message = 'Internal Error')
-	{
-		return $this->setStatusCode(500)->respondWithError($message);
-	}
-
 	public function respond($data, $headers = [])
 	{
 		return Response::json($data, $this->getStatusCode(), $headers);
-	}
-
-	public function respondWithPaginator($modelPaginated, $modelTransformed, $limit, $statusCode = 200)
-	{
-		return Response::json([
-			'data' => $modelTransformed,
-			'paginator' => [
-				'total_count' => $modelPaginated->total(),
-				'total_pages' => ceil($modelPaginated->total() / $limit),
-				'current_page' => $modelPaginated->currentPage(),
-				'limit' => $limit,
-			]
-		]);
 	}
 
 	public function respondWithError($message)
@@ -57,6 +40,40 @@ class ApiController extends BaseController {
 			]
 		]);
 	}
+
+	public function respondNotFound($message = 'Not found')
+	{
+		return $this->setStatusCode(404)->respondWithError($message);
+	}
+
+	public function respondInternalError($message = 'Internal Error')
+	{
+		return $this->setStatusCode(500)->respondWithError($message);
+	}
+
+	public function respondSuccess($message) 
+	{
+		return $this->setStatusCode(200)->respond([
+			'message' => $message,
+			'status_code' => $this->getStatusCode(),
+		]);
+	}
+
+	public function respondWithPaginator($modelPaginated, $modelTransformed, $limit)
+	{
+		return Response::json([
+			'status_code' => 200,
+			'data' => $modelTransformed,
+			'paginator' => [
+				'total_count' => $modelPaginated->total(),
+				'total_pages' => ceil($modelPaginated->total() / $limit),
+				'current_page' => $modelPaginated->currentPage(),
+				'limit' => $limit,
+			]
+		]);
+	}
+
+	
 }
 
  ?>
